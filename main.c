2eab0e25a31488656d3591974132caad3679448d
yves3a <yves.irakoz3@gmail.com>
2024-08-28 22:46:13 +0200

    hopefully will get to an  end

---
+++ main.c
@@ -0,0 +1,64 @@
+#include "s_shell.h"
+
+/**
+ * main -Entry point for the shell program
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
+ * Return: 0 on success, or the exit code of
+ * the last executed command.
+ */
+
+int main(int argc, char *argv[])
+{
+	size_t lenght = 0;
+	ssize_t read_num = 0;
+	s_shell_t *ptr = NULL;
+
+	/*Handle signal interrupts (e.g:Ctrl + C) by setting a custom signal handler*/
+	signal(SIGINT, handle_signalint);
+
+	/*Initialize the shell structure and set the program name*/
+	ptr = shell_init();
+	ptr->program_name = argv[0];
+	path_builder(&ptr->listof_path);
+
+	/**
+	 * If a file is passed as an argument, treat it as input
+	 * and execute commands from it
+	 */
+	if (argc >= 2)
+	{
+		file_as_input_handler(argv[1], ptr);
+	}
+
+	/*Main loop: continues running the shell until an exit condition is met*/
+
+	while (RUNNING)
+	{
+		prompt_shower();/*display the shell prompt*/
+		fflush(stdout);/*Ensure the prompt is displayed immediately*/
+
+		/*Read the next command line input*/
+
+		read_num = _getline(&ptr->line, &lenght, STDIN_FILENO);
+
+		/* Increment the command counter for each input received */
+		++ptr->counter_of_cmd;
+
+		/* check for empty prompt or if Ctrl+D to signal end of input*/
+		if (read_num == 0)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("exit\n");
+			exit_handler(ptr, free_multipler); /*clean up and exit the shell*/
+		}
+
+		/* Parse the input line and execute the command */
+		ptr->code_exiter = line_parsing(ptr);
+		safe_free(ptr->line); /*Free the memory allocated fo the input*/
+	}
+
+	/* Return the exit code from the last executed command */
+	return (ptr->code_exiter);
+}
